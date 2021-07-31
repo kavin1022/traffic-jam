ef3da0fdad034b065fce0f48704d69a772c4aba6
Pinghang Fan <pinghangfan1022@gmail.com>
2021-07-31 20:47:33 +1200

    Increased gap between button and text field on start and join page

--- frontend/src/pages/AnotherPage.js
+++ frontend/src/pages/AnotherPage.js
@@ -1,25 +1,29 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core';
 import '../styling/index.css';
 import InfoStats from '../components/TrafficPage/InfoStats';
 import Button from '@material-ui/core/Button';
 
-function startBreak() {
-  document.body.style = 'background: #bb1e10;';
-  console.log("test")
-}
-
-function finishBreak() {
-  document.body.style = 'background: #33a532;';
-}
-
 function AnotherPage() {
+
+  const [message, SetMessge] = useState("Work Away!");
+
+  function startBreak() {
+    document.body.style = 'background: #bb1e10;';
+    SetMessge("Time to break!");
+  }
+  
+  function finishBreak() {
+    document.body.style = 'background: #33a532;';
+    SetMessge("Work Away!");
+  }
 
   return (
     <div>
       <h1>Room Number: #28123</h1>
       <InfoStats/>
       <br/>
+      <p>{message}</p>
       <div className="three-lights">
         <button class="red" onClick={startBreak}></button>
         <span class="yellow"></span>
