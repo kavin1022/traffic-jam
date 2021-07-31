d8d52538a4d51fb12c711d09e082627c8e0be2e7
Pinghang Fan <pinghangfan1022@gmail.com>
2021-08-01 11:04:50 +1200

    changed

--- frontend/src/pages/AnotherPage.js
+++ frontend/src/pages/AnotherPage.js
@@ -1,19 +1,42 @@
+import { useState, useEffect } from 'react';
 import React from 'react';
 import { makeStyles } from '@material-ui/core';
 import '../styling/index.css';
 import InfoStats from '../components/TrafficPage/InfoStats';
 import Button from '@material-ui/core/Button';
 
-function startBreak() {
-  document.body.style = 'background: #bb1e10;';
-  console.log("test")
-}
-
-function finishBreak() {
-  document.body.style = 'background: #33a532;';
-}
-
 function AnotherPage() {
+  const [message, SetMessge] = useState("Click the green button to start!");
+  const [background, setBackground] = useState();
+
+  const red = () => {
+    document.body.style='background: #bb1e10;'
+    SetMessge("Time to break!");
+  }
+
+  const yellow = () => {
+    document.body.style='background: #fdbe5e;'
+    SetMessge("You have three seconds to save your file!");
+  }
+
+  const green = () => {
+    document.body.style='background: #33a532;'
+    SetMessge("Work Away!");
+  }
+
+  function startBreak() {
+    /* Sets background colour to yellow for five seconds before changing to red*/
+    setBackground(yellow);
+    setTimeout(() => {
+      setBackground(red)
+    }, 2000); 
+    
+  }
+  
+  function finishBreak() {
+    setBackground(green);
+    
+  }
 
   return (
     <div>
@@ -21,9 +44,9 @@ function AnotherPage() {
       <InfoStats/>
       <br/>
       <div className="three-lights">
-        <button class="red" onClick={startBreak}></button>
-        <span class="yellow"></span>
-        <button class="green" onClick={finishBreak}></button>
+        <button className="red" onClick={startBreak}></button>
+        <span className="yellow"></span>
+        <button className="green" onClick={finishBreak}></button>
       </div>
     </div>
 
--- frontend/src/utils/ExampleUtil.js
+++
@@ -1,3 +0,0 @@
-export function getMeaningOfLife() {
-  return 42;
-}
