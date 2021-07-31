5a6d78ac72a8c5795638ceb85dce5439d8263931
kavin1022 <pinghangfan1022@gmail.com>
2021-07-31 14:32:08 +1200

    updated a page name AnotherPage

---
+++ frontend/src/components/TrafficPage/InfoStats.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Info = () => {
+    return(
+        <h1>Info Display</h1>
+    )
+}
+export default Info
--- frontend/src/pages/AnotherPage.js
+++ frontend/src/pages/AnotherPage.js
@@ -1,19 +1,26 @@
 import React from 'react';
 import '../styling/index.css';
 import Button from '../components/Button';
-import { getMeaningOfLife } from '../utils/ExampleUtil';
+import InfoStats from '../components/TrafficPage/InfoStats';
 
-function getResult() {
-  alert(`The meaning of life: ${getMeaningOfLife()}`)
+function startBreak() {
+  document.body.style = 'background: #bb1e10;';
+}
+
+function finishBreak() {
+  document.body.style = 'background: #33a532;';
 }
 
 function AnotherPage() {
 
   return (
     <div>
-      <h1>This is another page!</h1>
-      <p>Go back to the previous page <a href={'/'}>here</a></p>
-      <Button buttonText={"Find out the meaning of life"} clickAction={getResult} />
+      <h1>Traffic-Jam</h1>
+      <InfoStats/>
+      <div>
+        <Button buttonText={"Break Time"} clickAction={startBreak} />
+        <Button buttonText={"Finish Break"} clickAction={finishBreak} />
+      </div>
     </div>
 
   );
