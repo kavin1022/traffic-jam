47f9daf80c97906e53668505363938edac7eeafd
FrancesXLiu <nlxliuxiaohan@gmail.com>
2021-08-01 13:04:30 +1200

    Start page and Join page: element centered

--- frontend/src/pages/JoinPage.js
+++ frontend/src/pages/JoinPage.js
@@ -4,6 +4,7 @@ import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+import '../styling/index-page.css';
 
 const StyledForm = styled(Button)`
 & {
